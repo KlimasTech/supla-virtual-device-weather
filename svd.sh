775cb5db250ebceeeb3f9cdc5c523b1e429d2058
KlimasTech <klimasstudio@gmail.com>
2023-04-12 23:37:39 +0200

    Create svd.sh

---
+++ svd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run Supla-Virtual-Device in screen
+cd /home/user/appdata/supla-virtual-device/
+screen -dmS Supla-Virtual-Device /home/user/appdata/supla-virtual-device/supla-virtual-device
