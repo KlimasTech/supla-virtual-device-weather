7732148bfc7db7ce3acba4671159fcc59bc5f281
KlimasTech <klimasstudio@gmail.com>
2023-04-15 18:03:12 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -9,6 +9,7 @@ API_KEY = '<openweathermapapi>'
 CITY_NAME = '<your-city>'
 BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}&units=metric'
 
+
 # Ustawienie ścieżki do folderu dla danych
 folder_path = '/home/user/appdata/supla-virtual-device/weather/data/'
 
@@ -39,35 +40,36 @@ dew_point = round((237.7 * math.log(humidity / 100.0) + (17.27 * temperature)) /
 visibility = round(data['visibility'] / 1000, 2)
 
 # Zapisanie wartości do plików tekstowych
-with open(os.path.join(folder_path, 'temperature.txt', 'w') as temp_file:
+with open(folder_path + 'temperature.txt', 'w') as temp_file:
     temp_file.write('{:.2f}\n'.format(temperature))
 
-with open(os.path.join(folder_path, 'humidity.txt', 'w') as hum_file:
+with open(folder_path + 'humidity.txt', 'w') as hum_file:
     hum_file.write('{}\n'.format(humidity))
 
-with open(os.path.join(folder_path, 'temp_hum.txt', 'w') as file:
-    file.write('{}\n{}\n'.format(temperature, humidity))
+with open(folder_path + 'temp_hum.txt', 'w') as temp_hum_file:
+    temp_hum_file.write('{}\n{}\n'.format(temperature, humidity))
 
-with open(os.path.join(folder_path, 'pressure.txt', 'w') as press_file:
+with open(folder_path + 'pressure.txt', 'w') as press_file:
     press_file.write('{}\n'.format(pressure))
 
-with open(os.path.join(folder_path, 'wind_speed.txt', 'w') as wind_file:
+with open(folder_path + 'wind_speed.txt', 'w') as wind_file:
     wind_file.write('{:.2f}\n'.format(wind_speed))
 
-with open(os.path.join(folder_path, 'dew_point.txt', 'w') as dew_file:
+with open(folder_path + 'dew_point.txt', 'w') as dew_file:
     dew_file.write('{:.2f}\n'.format(dew_point))
 
-with open(os.path.join(folder_path, 'visibility.txt', 'w') as vis_file:
+with open(folder_path + 'visibility.txt', 'w') as vis_file:
     vis_file.write('{:.2f}\n'.format(visibility))
 
-with open(os.path.join(folder_path, 'clouds.txt', 'w') as vis_file:
-    vis_file.write('{:.2f}\n'.format(clouds))
+with open(folder_path + 'clouds.txt', 'w') as clouds_file:
+    clouds_file.write('{:.2f}\n'.format(clouds))
 
-with open(os.path.join(folder_path, 'rain.txt', 'w') as vis_file:
-    vis_file.write('{:.2f}\n'.format(rain))
+with open(folder_path + 'rain.txt', 'w') as rain_file:
+    rain_file.write('{:.2f}\n'.format(rain))
 
-with open(os.path.join(folder_path, 'snow.txt', 'w') as vis_file:
-    vis_file.write('{:.2f}\n'.format(snow))
+with open(folder_path + 'snow.txt', 'w') as snow_file:
+    snow_file.write('{:.2f}\n'.format(snow))
 
-#with open('/home/user/appdata/supla-virtual-device/weather/data/uv_index.txt', 'w') as vis_file:
-#    vis_file.write('{:.2f}\n'.format(uv_index))
+# TO DO UV Index
+#with open(folder_path + 'uv_index.txt', 'w') as uv_file:
+#    uv_file.write('{:.2f}\n'.format(uv_index))
